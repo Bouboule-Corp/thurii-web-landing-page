95d0ad1720f24ace8db69ec57e7df1d5a44cc994
Lucas LAMOTHE <lucas@lamothe.eu>
2023-06-09 16:13:47 +0200

    feat: add Description section

---
+++ src/components/sections/Description.svelte
@@ -0,0 +1,77 @@
+<script lang="ts">
+  import sections from "../../sections.json";
+</script>
+
+<section id="description">
+    <div class="content">
+        <div class="text">
+            <h2>{ sections.description.title }</h2>
+            <p>{@html sections.description.description }</p>
+        </div>
+        <img src="/img/mockup_trip.svg" alt="Mockup iOS Application">
+    </div>
+</section>
+
+<style>
+    .content {
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        column-gap: 8rem;
+        padding: 0 6rem;
+    }
+
+    .content img {
+        width: 100%;
+        max-width: 32vw;
+    }
+
+    .content .text {
+        width: 38vw;
+    }
+
+    .content h2 {
+        margin-bottom: 1rem;
+        font-family: var(--font-title);
+        font-weight: var(--semi-bold);
+        font-size: 2rem;
+        color: var(--primary-default);
+    }
+
+    .content p {
+        font-family: var(--font-text);
+        font-weight: var(--regular);
+        font-size: 1rem;
+    }
+
+    @media (max-width: 1024px) {
+        .content {
+            padding: 0 4rem;
+        }
+
+        .content .text {
+            width: 100%;
+        }
+    }
+
+    @media (max-width: 768px) {
+        .content {
+            flex-direction: column;
+            row-gap: 0;
+            padding: 0 2rem;
+        }
+
+        .content img {
+            max-width: 90%;
+        }
+
+        .content .text {
+            height: 50%;
+        }
+
+        .content p {
+            font-size: .8rem;
+        }
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import Welcome from '$components/sections/Welcome.svelte';
     import Project from '../components/sections/Project.svelte';
+    import Description from '../components/sections/Description.svelte';
     import Timeline from '../components/sections/Timeline.svelte';
     import Team from '../components/sections/Team.svelte';
     import Contact from '../components/sections/Contact.svelte';
@@ -9,6 +10,7 @@
 <div class="Page">
     <Welcome />
     <Project />
+    <Description />
     <Timeline />
     <Team />
     <Contact />
