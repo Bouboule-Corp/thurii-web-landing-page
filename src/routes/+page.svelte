ba3aeddb06eb140da8ef014ec63f8277ff6fb65b
Lucas LAMOTHE <lucas@lamothe.eu>
2023-09-02 14:38:58 +0200

    feat: error message for screen with a height less than 500px

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,11 +7,39 @@
     import Contact from '../components/sections/Contact.svelte';
 </script>
 
-<div class="Page">
-    <Welcome />
-    <Project />
-    <Description />
-    <Timeline />
-    <Team />
-    <Contact />
+<div>
+    <div class="Page">
+        <Welcome />
+        <Project />
+        <Description />
+        <Timeline />
+        <Team />
+        <Contact />
+    </div>
+    <div class="Landscape">
+        <h1>This website was only visible on screen with a height of 500px or more.</h1>
+    </div>
 </div>
+
+<style>
+    .Landscape {
+        display: none;
+    }
+
+    @media only screen and (max-height: 500px) and (orientation: landscape) {
+        .Page {
+            display: none;
+        }
+
+        .Landscape {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+        }
+        .Landscape h1 {
+            font-size: 1.2rem;
+            text-align: center;
+        }
+    }
+</style>
